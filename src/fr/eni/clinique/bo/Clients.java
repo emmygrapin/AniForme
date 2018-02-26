f05ff563a9711c1d576f4f18b6cae66302698af8
AD\alemercier2017 <lemercier.alexandre1@gmail.com>
2018-02-26 14:58:02 +0100

    ALM : BO Clients

---
+++ src/fr/eni/clinique/bo/Clients.java
@@ -0,0 +1,163 @@
+package fr.eni.clinique.bo;
+
+import java.util.ArrayList;
+
+public class Clients {
+	
+	private String codeClient;
+	private String nomClient;
+	private String prenomClient;
+	private String adresse1;
+	private String adresse2;
+	private String codePostal;
+	private String ville;
+	private String numeroTel;
+	private String assurance;
+	private String email;
+	private String remarque;
+	private Boolean archive;
+	private ArrayList<Animaux> animaux;
+	
+	
+	public Clients(String codeClient, String nomClient, String prenomClient, String adresse1, String adresse2,
+			String codePostal, String ville, String numeroTel, String assurance, String email, String remarque,
+			Boolean archive, ArrayList<fr.eni.clinique.bo.Animaux> animaux) {
+		this.codeClient = codeClient;
+		this.nomClient = nomClient;
+		this.prenomClient = prenomClient;
+		this.adresse1 = adresse1;
+		this.adresse2 = adresse2;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.numeroTel = numeroTel;
+		this.assurance = assurance;
+		this.email = email;
+		this.remarque = remarque;
+		this.archive = archive;
+		this.animaux = animaux;
+	}
+
+	public Clients(String codeClient, String nomClient, String prenomClient, String adresse1, String adresse2,
+			String codePostal, String ville, String numeroTel, String assurance, String email, String remarque,
+			Boolean archive) {
+		this.codeClient = codeClient;
+		this.nomClient = nomClient;
+		this.prenomClient = prenomClient;
+		this.adresse1 = adresse1;
+		this.adresse2 = adresse2;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.numeroTel = numeroTel;
+		this.assurance = assurance;
+		this.email = email;
+		this.remarque = remarque;
+		this.archive = archive;
+		this.animaux = new ArrayList<Animaux>();
+	}
+	
+	public ArrayList<Animaux> getAnimaux() {
+		return animaux;
+	}
+
+	public void setAnimaux(ArrayList<Animaux> animaux) {
+		this.animaux = animaux;
+	}
+
+	public String getCodeClient() {
+		return codeClient;
+	}
+	
+	public void setCodeClient(String codeClient) {
+		this.codeClient = codeClient;
+	}
+	
+	public String getNomClient() {
+		return nomClient;
+	}
+	
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+	
+	public String getPrenomClient() {
+		return prenomClient;
+	}
+	
+	public void setPrenomClient(String prenomClient) {
+		this.prenomClient = prenomClient;
+	}
+	
+	public String getAdresse1() {
+		return adresse1;
+	}
+	
+	public void setAdresse1(String adresse1) {
+		this.adresse1 = adresse1;
+	}
+	
+	public String getAdresse2() {
+		return adresse2;
+	}
+	
+	public void setAdresse2(String adresse2) {
+		this.adresse2 = adresse2;
+	}
+	
+	public String getCodePostal() {
+		return codePostal;
+	}
+	
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	
+	public String getVille() {
+		return ville;
+	}
+	
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getNumeroTel() {
+		return numeroTel;
+	}
+	
+	public void setNumeroTel(String numeroTel) {
+		this.numeroTel = numeroTel;
+	}
+	
+	public String getAssurance() {
+		return this.assurance;
+	}
+	
+	public void setAssurance(String assurance) {
+		this.assurance = assurance;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getRemarque() {
+		return remarque;
+	}
+	
+	public void setRemarque(String remarque) {
+		this.remarque = remarque;
+	}
+	
+	public Boolean getArchive() {
+		return this.archive;
+	}
+	
+	public void setArchive(Boolean archive) {
+		this.archive = archive;
+	}
+	
+	
+
+}
