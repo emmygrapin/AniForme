f3273011e75ebf0e6ece7b6c234963876e6b9dcd
AD\mdeoliveira2017 <labulledemilho@gmail.com>
2018-03-02 12:05:39 +0100

    MD: ApplyController

--- src/fr/eni/clinique/ihm/ApplyController.java
+++ src/fr/eni/clinique/ihm/ApplyController.java
@@ -2,10 +2,11 @@ package fr.eni.clinique.ihm;
 
 import java.util.List;
 
-import javax.swing.JScrollPane;
-
-import fr.eni.clinique.bo.Personnel;
+import fr.eni.clinique.bo.Client;
 import fr.eni.clinique.dal.DALException;
+import fr.eni.clinique.ihm.ecranAnimal.AnimalController;
+import fr.eni.clinique.ihm.ecranPersonnel.PersonnelGestion;
+
 /**
  * 
  * @author egrapin2017
@@ -16,13 +17,25 @@ public class ApplyController {
 	public static ApplyController instance;
 	
 	private login ecr;
+	private clinique cli;
 	private int panel = 0;
 	
 	
 	
 	private ApplyController() throws DALException
 	{	
-		ecr = new login();}
+		AnimalController animalController = AnimalController.getInstance();
+		ClientController clientController = ClientController.getinstance();
+		//ecr = new login();
+		
+		cli = new clinique();
+		//cli.setContentPane(animalController.viewGestionAddAnimal());
+		PersonnelGestion ecranPersonnel = new PersonnelGestion();
+		ecranPersonnel.setVisible(true);
+		
+		//cli.setContentPane(clientController.globalClient(null));
+		
+	}
 	
 	public static ApplyController getInstance() throws DALException
 	{
@@ -33,5 +46,37 @@ public class ApplyController {
 		return ApplyController.instance;
 	}
 	
+	public void move(String menu, List liste) throws DALException
+	{
+		
+		ClientController clientController = ClientController.getinstance();
+		AnimalController animalController = AnimalController.getInstance();
+
+		switch(menu)
+
+		{
+			case "globalClient":
+				cli.setContentPane(clientController.globalClient((Client)liste.get(0)));
+			break;
+			
+			case "listAnim":
+				cli.setContentPane(animalController.viewAnimaux());
+			break;
+			case "newAnim":
+				cli.setContentPane(animalController.viewGestionAddAnimal());
+			break;
+			case "listePersonnels":
+				PersonnelGestion ecranPersonnel = new PersonnelGestion();
+				ecranPersonnel.setVisible(true);
+				break;
+				
+		}
+		
+		cli.validate();
+		cli.repaint();
+		
+//		ecr.validate();
+//		ecr.repaint();
+	}
 
 }
