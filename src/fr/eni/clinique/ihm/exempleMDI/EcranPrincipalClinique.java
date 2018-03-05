a17c263f97c06329b6ed581bf09704cf3cba2536
AD\egrapin2017 <grapin.emmy@gmail.com>
2018-03-05 17:22:56 +0100

    EG ecran principal

--- src/fr/eni/clinique/ihm/exempleMDI/EcranPrincipalClinique.java
+++ src/fr/eni/clinique/ihm/exempleMDI/EcranPrincipalClinique.java
@@ -12,11 +12,8 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
-import fr.eni.clinique.bo.Client;
 import fr.eni.clinique.dal.DALException;
 import fr.eni.clinique.ihm.login;
-import fr.eni.clinique.ihm.ecranAnimal.AnimalGestion;
-import fr.eni.clinique.ihm.ecranClient.InfosClient;
 import fr.eni.clinique.ihm.ecranPersonnel.PersonnelGestion;
 
 
@@ -24,21 +21,13 @@ public class EcranPrincipalClinique extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	
-
-	private static EcranPrincipalClinique instance;
-	
 	private JDesktopPane desktopPane;
 	private JMenuBar menuBarre;
 	private JMenu menuAgenda;
 	private PersonnelGestion ecranPersonnelGest;
-	private AnimalGestion ecranAnimalGest;
-
-	private InfosClient ecranClientGest;
-	private Client clientActif = null;
-	
 
 
-	private EcranPrincipalClinique() {
+	public EcranPrincipalClinique() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -57,40 +46,19 @@ public class EcranPrincipalClinique extends JFrame implements ActionListener {
 		//(écran enfant) faire pour tous les écrans
 		desktopPane.add(getEcranPersonnelGestion());
 
-		
-		desktopPane.add(getEcranInfosGestion());
-		
-
-		//desktopPane.add(getEcranAnimalGestion());
-
-
 	}
 
-	
-	public static EcranPrincipalClinique getInstance()
-	{
-		if( EcranPrincipalClinique.instance == null)
-		{
-			EcranPrincipalClinique.instance = new EcranPrincipalClinique();
-		}
-		return EcranPrincipalClinique.instance;
-	}
-	
 	// Lancement de l'application
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 
 			@Override
 			public void run() {
-				
-				EcranPrincipalClinique ecran;
-				ecran = EcranPrincipalClinique.getInstance();
-			
+				EcranPrincipalClinique ecran = new EcranPrincipalClinique();
 				//TODO dialog authentification
 				login loginEcran = new login(ecran);
 //				//TODO setVisible ici
 				ecran.setVisible(true);
-				
 			}
 		});
 
@@ -103,7 +71,7 @@ public class EcranPrincipalClinique extends JFrame implements ActionListener {
 		menuBarre.add(menu);
 
 		// Sous menu DÃ©connexion
-		JMenuItem menuItem = new JMenuItem("Déconnexion");
+		JMenuItem menuItem = new JMenuItem("DÃ©connexion");
 		menuItem.setActionCommand("deconnexion");
 		menuItem.addActionListener(this);
 		menu.add(menuItem);
@@ -115,24 +83,11 @@ public class EcranPrincipalClinique extends JFrame implements ActionListener {
 		menu.add(menuItem);
 
 		// Menu Agenda
-		menuItem = new JMenuItem("Gestion du personnel");
+		menuItem = new JMenuItem("Gestion Personnel");
 		menuBarre.add(menuItem);		
 		menuItem.setActionCommand("gestionPersonnel");
 		menuItem.addActionListener(this);
-		
 
-
-		menuItem = new JMenuItem("Gestion Client");
-		menuBarre.add(menuItem);		
-		menuItem.setActionCommand("gestionClient");
-		menuItem.addActionListener(this);
-
-//		// Menu Agenda
-//		menuItem = new JMenuItem("Gestion d'Animaux");
-//		menuBarre.add(menuItem);		
-//		menuItem.setActionCommand("gestionAnimal");
-//		menuItem.addActionListener(this);
-		
 	}
 
 	// RÃ©agir aux clicks sur les menus
@@ -148,13 +103,6 @@ public class EcranPrincipalClinique extends JFrame implements ActionListener {
 
 		case "gestionPersonnel":
 			getEcranPersonnelGestion().setVisible(true);
-			break;
-			
-		case "gestionClient":
-			getEcranInfosGestion().setVisible(true);
-			break;
-		case "gestionAnimal":
-			getEcranAnimalGestion().setVisible(true);
 			break;
 
 		default:
@@ -191,51 +139,5 @@ public class EcranPrincipalClinique extends JFrame implements ActionListener {
 		}
 		return ecranPersonnelGest;
 	}
-	
-	public InfosClient getEcranInfosGestion() {
-		if(ecranClientGest == null){
-			try {
-				ecranClientGest  = new InfosClient();
 
-			} catch (DALException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return ecranClientGest;
-	}
-		
-
-	
-	public Client getClientActif() {
-		return clientActif;
-	}
-
-	public void setClientActif(Client clientActif) {
-		this.clientActif = clientActif;
-	}
-	
-	public void refreshClientInfos()
-	{
-		ecranClientGest.setClientActif(clientActif);
-		ecranClientGest.refresh();
-	}
-	
-	/**
-	 * Getter 
-	 * @return
-	 */
-	public AnimalGestion getEcranAnimalGestion() {
-		if(ecranAnimalGest == null){
-			try {
-				ecranAnimalGest  = new AnimalGestion(this);
-			} catch (DALException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return ecranAnimalGest;
-	}
-	
-	
 }
