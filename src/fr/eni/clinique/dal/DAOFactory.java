e06a331d56ece1dff6461887170eecf99d78d005
AD\alemercier2017 <lemercier.alexandre1@gmail.com>
2018-03-06 14:17:15 +0100

    ALM : DAO Factory

--- src/fr/eni/clinique/dal/DAOFactory.java
+++ src/fr/eni/clinique/dal/DAOFactory.java
@@ -1,7 +1,10 @@
 package fr.eni.clinique.dal;
 
+import fr.eni.clinique.dal.jdbc.AnimalDAOJdbcImpl;
 import fr.eni.clinique.dal.jdbc.ClientDAOJdbcImpl;
+import fr.eni.clinique.dal.jdbc.ConnexionDAOJdbcImpl;
 import fr.eni.clinique.dal.jdbc.PersonnelDAOJdbcImpl;
+import fr.eni.clinique.dal.jdbc.RaceDAOJdbcImpl;
 /**
  * 
  * @author egrapin2017
@@ -17,6 +20,11 @@ public class DAOFactory {
 	public static AnimalDAO getAnimalDAO() {
 		AnimalDAO animalDAO = new AnimalDAOJdbcImpl();
 		return animalDAO;
+	}
+	
+	public static ConnexionDAO getConnexionDAO() {
+		ConnexionDAO connexionDAO = new ConnexionDAOJdbcImpl();
+		return connexionDAO;
 	}
 
 	public static ClientDAO getClientDAO() {
